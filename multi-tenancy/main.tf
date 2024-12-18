7a195cf00c1796cf5f46400078eb1de8a9e1168a
Devendraappa <devendraappa.kune@gmail.com>
2024-12-18 12:56:51 +0530

    Update main.tf

--- multi-tenancy/main.tf
+++ multi-tenancy/main.tf
@@ -94,15 +94,3 @@ resource "aws_security_group" "allow_ssh" {
     Name = "allow_ssh"
   }
 }
-
-# EC2 Instance
-resource "aws_instance" "web" {
-  ami           = "ami-053b12d3152c0cc71"
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.public.id
-  security_groups = [aws_security_group.allow_ssh.name]
-
-  tags = {
-    Name = "web-instance"
-  }
-}
