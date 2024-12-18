856f147942d65287a6b367c5d3fa673c0e82fb72
Devendraappa <devendraappa.kune@gmail.com>
2024-12-18 09:25:12 +0530

    Update remote-backend.tf

--- remote-backend.tf
+++ remote-backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "training-test678"
+    bucket         = "terraform-bucket-789"
     key            = "terraform.tfstate"
     region         = "ap-south-1"
     encrypt        = true
